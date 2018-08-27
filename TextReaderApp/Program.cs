9eda619265ffb116c387ed9a9ee600ba47d74973
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-08-27 15:32:09 +0300

    1

--- TextReaderApp/Program.cs
+++ TextReaderApp/Program.cs
@@ -7,24 +7,11 @@ namespace TextReaderApp
     {
         static void Main(string[] args)
         {
-            int i=0;
-            string dir;
             Console.WriteLine("Enter a directory");
-            dir = Console.ReadLine();
-            DirectoryInfo d = new DirectoryInfo(dir);
-            FileInfo[] Files = d.GetFiles("*.txt");
+            TextReader TxtReader = new TextReader(Console.ReadLine());
+            TxtReader.GetFilesFromDir();
             Console.WriteLine("Choose a file that needs to be read");
-            foreach (FileInfo file in Files)
-                Console.WriteLine("{0} - {1}", ++i, file.Name);
-            i=Convert.ToInt32(Console.ReadLine());
-            StreamReader reader = new StreamReader(Files[--i].FullName);
-            string line = reader.ReadLine();
-            while (line != null)
-            {
-                Console.WriteLine(line);
-                line = reader.ReadLine();
-            }
-            reader.Close();
+            TxtReader.GetTextFromFile(Convert.ToInt32(Console.ReadLine()));
             Console.ReadKey();
         }
     }
---
+++ TextReaderApp/TextReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextReaderApp
+{
+    class TextReader
+    {
+        private string _Dir;
+        private FileInfo[] Files;
+
+        public string Dir { get; set; }
+
+        public TextReader(string dir)
+        {
+            Dir = dir;
+        }
+
+        public void GetFilesFromDir()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Dir);
+            Files = dir.GetFiles("*.txt");
+            int i=0;
+            foreach (FileInfo file in Files)
+                Console.WriteLine("{0} - {1}", ++i, file.Name);
+        }
+
+        public void GetTextFromFile(int item)
+        {
+            StreamReader reader = new StreamReader(Files[--item].FullName);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+    }
+}
